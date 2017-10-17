4da456fd0b1d57288dc57bebbc228885971307da
sw-yx <swyx@swyx.io>
2017-10-17 17:50:11 -0400

    add localstorage

--- client/store/index.js
+++ client/store/index.js
@@ -1,16 +1,24 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { compose, createStore, combineReducers, applyMiddleware } from "redux";
 import createLogger from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import user from "./user";
 import gamestate from "./gamestate";
 import modalstate from "./modalstate";
+import persistState from "redux-localstorage";
 
 const reducer = combineReducers({ user, gamestate, modalstate });
+// const middleware = applyMiddleware(
+//   thunkMiddleware,
+//   createLogger({ collapsed: true })
+// );
 const middleware = applyMiddleware(
   thunkMiddleware,
   createLogger({ collapsed: true })
 );
-const store = createStore(reducer, middleware);
+
+const enhancer = compose(persistState());
+
+const store = createStore(reducer, middleware, enhancer);
 
 export default store;
 export * from "./user";
