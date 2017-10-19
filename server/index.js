ae6e7eff245a25f0c8c92d3398cc9fb9cebf7fd6
sw-yx <swyx@swyx.io>
2017-10-19 01:57:59 -0400

    refactor server to use swyx module

--- server/index.js
+++ server/index.js
@@ -1,25 +1,21 @@
 const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
 const compression = require("compression");
 const session = require("express-session");
 const passport = require("passport");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const db = require("./db");
 const sessionStore = new SequelizeStore({ db });
-const PORT = process.env.PORT || 8080;
-const app = express();
-// const socketio = require("socket.io");
-module.exports = app;
-
-// This package will handle GraphQL server requests and responses
-// for you, based on your schema.
 const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
 
 const schema = require("../schema");
 
-/**
+const server = require("swyx")();
+const app = server.app;
+
+module.exports = app;
+
+const createApp = () => {
+  /**
  * In your development environment, you can keep all of your
  * app's secret API keys in a file called `secrets.js`, in your project
  * root. This file is included in the .gitignore - it will NOT be tracked
@@ -27,22 +23,18 @@ const schema = require("../schema");
  * keys as environment variables, so that they can still be read by the
  * Node process on process.env
  */
-if (process.env.NODE_ENV !== "production") require("../secrets");
+  if (process.env.NODE_ENV !== "production") require("../secrets");
 
-// passport registration
-passport.serializeUser((user, done) => done(null, user.id));
-passport.deserializeUser((id, done) =>
-  db.models.user.findById(id).then(user => done(null, user)).catch(done)
-);
-
-const createApp = () => {
-  // logging middleware
-  app.use(morgan("dev"));
-
-  // body parsing middleware
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-
+  // auth and api routes
+  app.use("/graphql", graphqlExpress({ schema }));
+  app.use(
+    "/graphiql",
+    graphiqlExpress({
+      endpointURL: "/graphql"
+    })
+  );
+  // app.use("/auth", require("./auth"));
+  // app.use("/api", require("./api"));
   // compression middleware
   app.use(compression());
 
@@ -55,45 +47,12 @@ const createApp = () => {
       saveUninitialized: false
     })
   );
-  app.use(passport.initialize());
-  app.use(passport.session());
 
-  // auth and api routes
-  app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
-  app.use(
-    "/graphiql",
-    graphiqlExpress({
-      endpointURL: "/graphql"
-    })
-  );
-  app.use("/auth", require("./auth"));
-  app.use("/api", require("./api"));
-
-  // static file-serving middleware
-  app.use(express.static(path.join(__dirname, "..", "public")));
-
-  // sends index.html
-  app.use("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "public/index.html"));
-  });
-
-  // error handling endware
-  app.use((err, req, res, next) => {
-    console.error(err);
-    console.error(err.stack);
-    res.status(err.status || 500).send(err.message || "Internal server error.");
-  });
+  app.use(server.finalHandler);
 };
 
 const startListening = () => {
-  // start listening (and create a 'server' object representing our server)
-  const server = app.listen(PORT, () =>
-    console.log(`Mixing it up on port ${PORT}`)
-  );
-
-  // set up our socket control center
-  // const io = socketio(server);
-  // require("./socket")(io);
+  server.start();
 };
 
 const syncDb = () => db.sync();
